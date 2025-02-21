452a2b211fefa255b743cf7f7479efc712ff171e
dambai212 <damanybailey@icloud.com>
2025-02-20 23:04:42 -0500

    Fix CORS configuration to handle missing CORS_DOMAINS environment variable

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, logger: (-> { Rails.logger }) do
   allow do
-    origins ENV["CORS_DOMAINS"].split(",").map { |d| /#{d}/ }
-    resource(
+    origins (ENV["CORS_DOMAINS"] || "").split(",").map { |d| /#{d}/ }
+      resource(
       "/api/*",
       headers: :any,
       methods: %i[get options head],
